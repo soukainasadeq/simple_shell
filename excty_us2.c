f75125acf50d49d394c96aa7ff756f4d45302ade
soukainasadeq <soukainasadeq.15@gmail.com>
2023-08-23 15:37:37 -0700

    siK

--- excty_us2.c
+++ excty_us2.c
@@ -1,157 +1,162 @@
 #include "main.h"
 
 /**
- * exit_sh - BBBBB BBBBBBNNN
- * @datashell: DBBb bbbbbbbbjjjjjjjjj jjjjjjjjj hhhhha
- *             mmmm hhhhhhhhh bb
+ * exit_sh - Exits the shell.
+ * @datashell: Data structure for the shell.
  *
- * Return: zzz z  z zzzz
+ * Return: Returns the exit status.
  */
 int exit_sh(shll_comm *datashell)
 {
-	unsigned int ustt;
-	int isdgt, strln, bign;
+    unsigned int exit_status;
+    int is_digit, str_len, is_big_number;
 
-	if (datashell->args[1] != NULL)
-	{
-		ustt = _atoi(datashell->args[1]);
-		isdgt = _isdigit(datashell->args[1]);
-		strln = _strlen(datashell->args[1]);
-		bign = ustt > (unsigned int)INT_MAX;
-		if (!isdgt || strln > 10 || bign)
-		{
-			get_err(datashell, 2);
-			datashell->stat = 2;
-			return (1);
-		}
-		datashell->stat = (ustt % 256);
-	}
-	return (0);
+    if (datashell->args[1] != NULL)
+    {
+        exit_status = _atoi(datashell->args[1]);
+        is_digit = _isdigit(datashell->args[1]);
+        str_len = _strlen(datashell->args[1]);
+        is_big_number = exit_status > (unsigned int)INT_MAX;
+
+        if (!is_digit || str_len > 10 || is_big_number)
+        {
+            get_error(datashell, 2);
+            datashell->stat = 2;
+            return (1);
+        }
+
+        datashell->stat = (exit_status % 256);
+    }
+    return (0);
 }
 
 /**
- * execute_line - cccccnnn nnnnnnnnnnn nnnnnnnn
- * @datash: zzzzzz zzzzzz zzzzzzz
+ * execute_line - Executes a command line.
+ * @datash: Data structure for the shell.
  *
- * Return: zzzzzzzzz zzz
+ * Return: Returns 1 on success, 0 on failure.
  */
 int execute_line(shll_comm *datash)
 {
-	int (*builtin)(shll_comm *datash);
+    int (*builtin)(shll_comm *datash);
 
-	if (datash->args[0] == NULL)
-		return (1);
+    if (datash->args[0] == NULL)
+        return (1);
 
-	builtin = get_bltn(datash->args[0]);
+    builtin = get_builtin(datash->args[0]);
 
-	if (builtin != NULL)
-		return (builtin(datash));
+    if (builtin != NULL)
+        return (builtin(datash));
 
-	return (command_exec(datash));
+    return (command_exec(datash));
 }
 
 /**
- * err_environ - gggggggggg ggggggggg ggggggggggg gggggggggg gggggg ggg
- * @data_sh: ddddddddddddd   dddddddddddddddd d ddddddddddddd
+ * err_environ - Generates an error message related to environment variables.
+ * @data_sh: Data structure for the shell.
  *
- * Return: eeeeeeeeee eeee eeeee
+ * Return: Returns the error message.
  */
 char *err_environ(shll_comm *data_sh)
 {
-	int leng;
-	char *er, *txt, *vst;
+    int length;
+    char *error, *text, *vstr;
 
-	vst = conv_itoa(data_sh->counter);
-	txt = ": Unable to add/remove from environment\n";
-	leng = _strlen(data_sh->argv[0]) + _strlen(vst);
-	leng += _strlen(data_sh->args[0]) + _strlen(txt) + 4;
-	er = malloc(sizeof(char) * (leng + 1));
-	if (er == 0)
-	{
-		free(er);
-		free(vst);
-		return (NULL);
-	}
+    vstr = conv_itoa(data_sh->counter);
+    text = ": Unable to add/remove from environment\n";
+    length = _strlen(data_sh->argv[0]) + _strlen(vstr);
+    length += _strlen(data_sh->args[0]) + _strlen(text) + 4;
 
-	_strcpy(er, data_sh->argv[0]);
-	_strcat(er, ": ");
-	_strcat(er, vst);
-	_strcat(er, ": ");
-	_strcat(er, data_sh->args[0]);
-	_strcat(er, txt);
-	_strcat(er, "\0");
-	free(vst);
+    error = malloc(sizeof(char) * (length + 1));
+    if (error == 0)
+    {
+        free(error);
+        free(vstr);
+        return (NULL);
+    }
 
-	return (er);
+    _strcpy(error, data_sh->argv[0]);
+    _strcat(error, ": ");
+    _strcat(error, vstr);
+    _strcat(error, ": ");
+    _strcat(error, data_sh->args[0]);
+    _strcat(error, text);
+    _strcat(error, "\0");
+    free(vstr);
+
+    return (error);
 }
 
 /**
- * err_path126 - bbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbb
- *                opooooooooooooooo ooo oooooooooooo
- * @data_sh: dgggggggbbbbbbbbbbbb gbbbbbbbbg bgb bgggbbbbgbbb
+ * err_path126 - Generates an error message for permission denied.
+ * @data_sh: Data structure for the shell.
  *
- * Return: Tdddddd dddddddd dddddddd
+ * Return: Returns the error message.
  */
 char *err_path126(shll_comm *data_sh)
 {
-	int leng;
-	char *vst, *er;
+    int length;
+    char *vstr, *error;
 
-	vst = conv_itoa(data_sh->counter);
-	leng = _strlen(data_sh->argv[0]) + _strlen(vst);
-	leng += _strlen(data_sh->args[0]) + 24;
-	er = malloc(sizeof(char) * (leng + 1));
-	if (er == 0)
-	{
-		free(er);
-		free(vst);
-		return (NULL);
-	}
-	_strcpy(er, data_sh->argv[0]);
-	_strcat(er, ": ");
-	_strcat(er, vst);
-	_strcat(er, ": ");
-	_strcat(er, data_sh->args[0]);
-	_strcat(er, ": Permission denied\n");
-	_strcat(er, "\0");
-	free(vst);
-	return (er);
+    vstr = conv_itoa(data_sh->counter);
+    length = _strlen(data_sh->argv[0]) + _strlen(vstr);
+    length += _strlen(data_sh->args[0]) + 24;
+
+    error = malloc(sizeof(char) * (length + 1));
+    if (error == 0)
+    {
+        free(error);
+        free(vstr);
+        return (NULL);
+    }
+
+    _strcpy(error, data_sh->argv[0]);
+    _strcat(error, ": ");
+    _strcat(error, vstr);
+    _strcat(error, ": ");
+    _strcat(error, data_sh->args[0]);
+    _strcat(error, ": Permission denied\n");
+    _strcat(error, "\0");
+    free(vstr);
+
+    return (error);
 }
 
 /**
- * conc_err_msg - cccccccccccc c ccccccccccccccccccccccccccccccc cccc
- * @data_sh: vvvvvvv bbbbbbbbbbbb bbbbbbbbbbbb bbbbbbbbb bbbbbj
- * @disp_msg: mmmmmmmmmm dkkkkkkkkkd k
- * @err: zzzzzzz zzzzzzz zzzzzz
- * @vstr: cccc cccccnes.
+ * conc_err_msg - Concatenates error message.
+ * @data_sh: Data structure for the shell.
+ * @disp_msg: Display message.
+ * @err: Error message.
+ * @vstr: Version string.
  *
- * Return: lllllll llll llll lllll llll ll
+ * Return: Returns the concatenated error message.
  */
 char *conc_err_msg(shll_comm *data_sh, char *disp_msg, char *err, char *vstr)
 {
-	char *ilegal_flg;
+    char *illegal_flag;
 
-	_strcpy(err, data_sh->argv[0]);
-	_strcat(err, ": ");
-	_strcat(err, vstr);
-	_strcat(err, ": ");
-	_strcat(err, data_sh->args[0]);
-	_strcat(err, disp_msg);
-	if (data_sh->args[1][0] == '-')
-	{
-		ilegal_flg = malloc(3);
-		ilegal_flg[0] = '-';
-		ilegal_flg[1] = data_sh->args[1][1];
-		ilegal_flg[2] = '\0';
-		_strcat(err, ilegal_flg);
-		free(ilegal_flg);
-	}
-	else
-	{
-		_strcat(err, data_sh->args[1]);
-	}
+    _strcpy(err, data_sh->argv[0]);
+    _strcat(err, ": ");
+    _strcat(err, vstr);
+    _strcat(err, ": ");
+    _strcat(err, data_sh->args[0]);
+    _strcat(err, disp_msg);
 
-	_strcat(err, "\n");
-	_strcat(err, "\0");
-	return (err);
+    if (data_sh->args[1][0] == '-')
+    {
+        illegal_flag = malloc(3);
+        illegal_flag[0] = '-';
+        illegal_flag[1] = data_sh->args[1][1];
+        illegal_flag[2] = '\0';
+        _strcat(err, illegal_flag);
+        free(illegal_flag);
+    }
+    else
+    {
+        _strcat(err, data_sh->args[1]);
+    }
+
+    _strcat(err, "\n");
+    _strcat(err, "\0");
+    return (err);
 }
--- help-us2.c
+++ help-us2.c
@@ -96,4 +96,102 @@ void get_ln(char **lnptr, size_t *num, char *buf, size_t size_b)
 		_strcpy(*lnptr, buf);
 		free(buf);
 	}
+#include "main.h"
+
+/**
+ *  * disp_help_general - ggggggggggg gggjjgggdgggg gg gggggggggggg gggggggg ggggg
+ *   *
+ *    * Return: Vggg
+ *     */
+void disp_help_general(void)
+{
+	char *hep = "^-^ bash, version 1.0(1)-release\n";
+
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "These commands are defined internally.Type 'help' to see the list";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "Type 'help name' to find out more about the function 'name'.\n\n ";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = " alias: alias [name=['string']]\n cd: cd [-L|[-P [-e]] [-@]] ";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "[dir]\nexit: exit [n]\n  env: env [option] [name=value] [command ";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "[args]]\n  setenv: setenv [variable] [value]\n  unsetenv: ";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "unsetenv [variable]\n";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+}
+/**
+ *  * disp_help_exit - hhhhhhhhhhhhhhhhh hhhhhhhhhhh vavhbjt
+ *   *
+ *    * Return: Vddd
+ *     */
+void disp_help_exit(void)
+{
+	char *hep = "exit: exit [n]\n Exit shell.\n";
+
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "Exits the shell with a status of N. If N is ommited, the exit";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "statusis that of the last command executed\n";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+}
+
+/**
+ *  * display_unsetenv - ccccccccc cccbbbbb bbbbbbbbbb bbbbbbbbbbn
+ *   *
+ *    * Return: Vxxx
+ *     */
+void display_unsetenv(void)
+{
+	char *hep = "unsetenv: unsetenv (const char *name)\n\t";
+
+	write(STDOUT_FILENO, hep, _strlen(hep));
+	hep = "Remove an entry completely from the environment\n";
+	write(STDOUT_FILENO, hep, _strlen(hep));
+}
+
+/**
+ *  * get_sigint - xxxxxxxxxxx xxxxxxxx xxxxxxxxxxx x
+ *   * @sig: xxxxxxxxxndler
+ *    */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
+
+/**
+ *  * get_ln - zzzzzzzzz zzzzzz z zzzzzzzzzzzzze
+ *   * @lnptr: Pzzzzzzzzzz zzzzzzzzzariable
+ *    * @num: Poinzzzzzzzzz zzzzz of zzzzzzzzzzble
+ *     * @buf: String thabbbbb bbbbbbbbbbbb bbbbbbbe
+ *      * @size_b: gggggg ggggg bbbbr
+ *       * Return: nothing
+ *        */
+void get_ln(char **lnptr, size_t *num, char *buf, size_t size_b)
+{
+
+	if (*lnptr == NULL)
+	{
+			if (size_b > BUFSIZE)
+				*num = size_b;
+	
+			else
+				*num = BUFSIZE;
+			*lnptr = buf;
+	}
+	else if (*num < size_b)
+	{
+			if (size_b > BUFSIZE)
+				*num = size_b;
+			else
+				*num = BUFSIZE;
+			*lnptr = buf;
+	}
+	else
+	{
+			_strcpy(*lnptr, buf);
+			free(buf);
+	}
 }
